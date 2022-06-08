776be9887f439ad5d1499587f1dd0a4a44a53464
Enes <54986652+altunenes@users.noreply.github.com>
2022-06-08 18:03:29 +0300

    Update JSsourceCode.js

--- JSsourceCode.js
+++ JSsourceCode.js
@@ -151,7 +151,64 @@ clearButton.onclick = function() {
   location.reload();
 };
 
-
+var scrambleCircularAreaButton = document.createElement('button');
+scrambleCircularAreaButton.innerHTML = 'Scramble Circular Area';
+scrambleCircularAreaButton.onclick = function() {
+  var image = document.getElementsByTagName('img')[0];
+  var canvas = document.createElement('canvas');
+  canvas.width = image.width;
+  canvas.height = image.height;
+  var context = canvas.getContext('2d');
+  context.drawImage(image, 0, 0);
+  var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+  var data = imageData.data;
+  var x = 0;
+  var y = 0;
+  var radius = 0;
+  var isDrawing = false;
+  canvas.onmousedown = function(e) {
+    isDrawing = true;
+    x = e.offsetX;
+    y = e.offsetY;
+    radius = 0;
+  };
+  canvas.onmousemove = function(e) {
+    if (isDrawing) {
+      radius = Math.sqrt(Math.pow(e.offsetX - x, 2) + Math.pow(e.offsetY - y, 2));
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.drawImage(image, 0, 0);
+      context.beginPath();
+      context.arc(x, y, radius, 0, 2 * Math.PI);
+      context.stroke();
+    }
+  };
+  canvas.onmouseup = function(e) {
+    isDrawing = false;
+    var pixels = [];
+    for (var i = 0; i < data.length; i += 4) {
+      var dx = (i / 4) % canvas.width - x;
+      var dy = Math.floor((i / 4) / canvas.width) - y;
+      if (Math.sqrt(dx * dx + dy * dy) < radius) {
+        pixels.push(i);
+      }
+    }
+    for (var i = 0; i < pixels.length; i++) {
+      var j = Math.floor(Math.random() * pixels.length);
+      var temp = data[pixels[i]];
+      data[pixels[i]] = data[pixels[j]];
+      data[pixels[j]] = temp;
+      temp = data[pixels[i] + 1];
+      data[pixels[i] + 1] = data[pixels[j] + 1];
+      data[pixels[j] + 1] = temp;
+      temp = data[pixels[i] + 2];
+      data[pixels[i] + 2] = data[pixels[j] + 2];
+      data[pixels[j] + 2] = temp;
+    }
+    context.putImageData(imageData, 0, 0);
+  };
+  document.body.appendChild(canvas);
+};
+document.body.appendChild(scrambleCircularAreaButton);
 
 var buttons = document.querySelectorAll('button');
 for (var i = 0; i < buttons.length; i++) {
