afa1d0ba3e3f12f1bbad1aea7b7e1ea695cd883f
altunenes <enesaltun2@gmail.com>
2023-10-21 19:16:13 +0300

    fourier scramble

--- docs/js/scramblery.js
+++ docs/js/scramblery.js
@@ -1,8 +1,4 @@
-let isCvReady = false;
-function onOpenCvReady() {
-    isCvReady = true;
-    document.getElementById('status').innerHTML = 'OpenCV.js is ready.';
-}
+
 document.addEventListener("DOMContentLoaded", function() {
   let originalImage; 
   let processedImage; 
@@ -14,15 +10,43 @@ document.addEventListener("DOMContentLoaded", function() {
   document.body.style.alignItems = 'center';
   document.body.style.justifyContent = 'flex-start';
   document.body.style.overflow = 'auto';
+  const headerContainer = document.createElement('div');
+  headerContainer.style.display = 'flex';
+  headerContainer.style.alignItems = 'center';
+  headerContainer.style.justifyContent = 'space-between';
+  headerContainer.style.width = '100%';
+  headerContainer.style.backgroundColor = '#6200ee';
+  
   const header = document.createElement('h1');
   header.textContent = 'Scramblery';
   header.style.textAlign = 'center';
-  header.style.width = '100%';
-  header.style.backgroundColor = '#6200ee';
-  header.style.color = 'white';
   header.style.margin = '0';
   header.style.padding = '10px 0';
-  document.body.appendChild(header);
+  header.style.flexGrow = '1';
+  header.style.color = 'White';
+
+  headerContainer.appendChild(header);
+  
+  const githubLink = document.createElement('a');
+  githubLink.href = 'https://github.com/altunenes/scramblery';
+  githubLink.textContent = 'View on GitHub';
+  githubLink.style.color = 'white';
+  githubLink.style.marginRight = '20px';
+  githubLink.style.textDecoration = 'none';
+  githubLink.style.fontSize = '16px';
+  githubLink.style.border = '1px solid white';
+  githubLink.style.padding = '5px 10px';
+  githubLink.style.borderRadius = '5px';
+  githubLink.style.transition = 'background-color 0.3s';
+  githubLink.addEventListener('mouseenter', () => {
+      githubLink.style.backgroundColor = 'rgba(255, 255, 255, 0.2)';
+  });
+  githubLink.addEventListener('mouseleave', () => {
+      githubLink.style.backgroundColor = 'transparent';
+  });
+  headerContainer.appendChild(githubLink);
+  
+  document.body.insertBefore(headerContainer, document.body.firstChild);
 
   const controlsContainer = document.createElement('div');
   controlsContainer.style.display = 'flex';
@@ -134,7 +158,7 @@ document.addEventListener("DOMContentLoaded", function() {
       createProcessedImage(canvas.toDataURL());
   });
 
-  createButton('Scramble Image', function() {
+  createButton('Tile Shuffle', function() {
       if (!originalImage) return; 
 
       const canvas = document.createElement('canvas');
@@ -167,109 +191,54 @@ document.addEventListener("DOMContentLoaded", function() {
 
       createProcessedImage(canvas.toDataURL());
   });
+  createButton('Pixel Scramble', function() {
+    if (!originalImage) return; 
+
+    const canvas = document.createElement('canvas');
+    canvas.width = originalImage.width;
+    canvas.height = originalImage.height;
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(originalImage, 0, 0);
+
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data;
+    const mosaicSize = parseInt(document.getElementById('mosaicSize').value);
+
+    for (let i = 0; i < data.length; i += 4) {
+        const x = (i / 4) % canvas.width;
+        const y = Math.floor((i / 4) / canvas.width);
+        if (x % mosaicSize === 0 && y % mosaicSize === 0) {
+            for (let j = 0; j < mosaicSize; j++) {
+                for (let k = 0; k < mosaicSize; k++) {
+                    const index = (y + j) * canvas.width + (x + k);
+                    data[index * 4] = data[i];
+                    data[index * 4 + 1] = data[i + 1];
+                    data[index * 4 + 2] = data[i + 2];
+                    data[index * 4 + 3] = data[i + 3];
+                }
+            }
+        }
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+
+    createProcessedImage(canvas.toDataURL());
+});
 
   createButton('Clear', function() {
       location.reload(); 
   });
 
-  createButton('Scramble Mosaic', function() {
-      if (!originalImage) return; 
-
-      const canvas = document.createElement('canvas');
-      canvas.width = originalImage.width;
-      canvas.height = originalImage.height;
-      const ctx = canvas.getContext('2d');
-      ctx.drawImage(originalImage, 0, 0);
-
-      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-      const data = imageData.data;
-      const mosaicSize = parseInt(document.getElementById('mosaicSize').value);
-
-      for (let i = 0; i < data.length; i += 4) {
-          const x = (i / 4) % canvas.width;
-          const y = Math.floor((i / 4) / canvas.width);
-          if (x % mosaicSize === 0 && y % mosaicSize === 0) {
-              for (let j = 0; j < mosaicSize; j++) {
-                  for (let k = 0; k < mosaicSize; k++) {
-                      const index = (y + j) * canvas.width + (x + k);
-                      data[index * 4] = data[i];
-                      data[index * 4 + 1] = data[i + 1];
-                      data[index * 4 + 2] = data[i + 2];
-                      data[index * 4 + 3] = data[i + 3];
-                  }
-              }
-          }
-      }
-
-      ctx.putImageData(imageData, 0, 0);
-
-      createProcessedImage(canvas.toDataURL());
-  });
-  const statusIndicator = document.createElement('div');
-  statusIndicator.id = 'status';
-  statusIndicator.textContent = 'Loading OpenCV.js...';
-  document.body.appendChild(statusIndicator);
-
-
-  
-  createButton('Fourier Scramble', function() {
-    if (!originalImage || !isCvReady) {
-      alert('Please wait for OpenCV to load, or make sure an image is loaded.');
-      return;
+  const downloadLink = document.createElement('a');
+const downloadBtn = createButton('Download', function() {
+    if (processedImage) {
+        downloadLink.href = processedImage.src;
+        downloadLink.download = 'processed_image.png';
+        downloadLink.click();
     }
-
-    const canvas = document.createElement('canvas');
-    canvas.width = originalImage.width;
-    canvas.height = originalImage.height;
-    const context = canvas.getContext('2d');
-    context.drawImage(originalImage, 0, 0);
-
-    let imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-
-    let gray = cv.imread(imageData);
-    cv.cvtColor(gray, gray, cv.COLOR_RGBA2GRAY);
-
-    let scrambledImage = fourierTransformAndScramble(gray);
-    
-    let resultImageData = new ImageData(new Uint8ClampedArray(scrambledImage.data), gray.cols, gray.rows);
-
-    context.putImageData(resultImageData, 0, 0);
-    createProcessedImage(canvas.toDataURL());
-
-    gray.delete(); scrambledImage.delete();
 });
-
-function fourierTransformAndScramble(mat) {
-    const optimalRows = cv.getOptimalDFTSize(mat.rows);
-    const optimalCols = cv.getOptimalDFTSize(mat.cols);
-    let padded = new cv.Mat();
-    cv.copyMakeBorder(mat, padded, 0, optimalRows - mat.rows, 0, optimalCols - mat.cols, cv.BORDER_CONSTANT, new cv.Scalar());
-
-    padded.convertTo(padded, cv.CV_32F);
-
-    let planes = new cv.MatVector();
-    planes.push_back(padded);
-    planes.push_back(new cv.Mat.zeros(padded.rows, padded.cols, cv.CV_32F));
-    let complexI = new cv.Mat();
-    cv.merge(planes, complexI);
-    cv.dft(complexI, complexI);
-
-    let mag = new cv.Mat(), angle = new cv.Mat();
-    cv.cartToPolar(planes.get(0), planes.get(1), mag, angle, false);
-    let randomPhase = new cv.Mat(angle.rows, angle.cols, cv.CV_32F, new cv.Scalar(Math.PI));
-    cv.addWeighted(angle, 1, randomPhase, 1, 0, angle, -1);
-    cv.polarToCart(mag, angle, planes.get(0), planes.get(1), false);
-
-    cv.merge(planes, complexI);
-    cv.idft(complexI, complexI, cv.DFT_REAL_OUTPUT);
-    let restored = new cv.Mat();
-    cv.split(complexI, planes);
-    planes.get(0).convertTo(restored, cv.CV_8U);
-
-    padded.delete(); planes.delete(); complexI.delete(); mag.delete(); angle.delete(); randomPhase.delete();
-
-    return restored;
-}
+controlsContainer.appendChild(downloadLink);
+downloadLink.appendChild(downloadBtn);
   function createProcessedImage(dataUrl) {
       if (processedImage) {
           processedImageContainer.removeChild(processedImage);
@@ -281,6 +250,8 @@ function fourierTransformAndScramble(mat) {
       processedImage.style.maxHeight = '100%';
       processedImageContainer.appendChild(processedImage);
   }
+
+  
 });
 
 
