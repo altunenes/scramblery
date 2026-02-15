5b04aa1388d4ad5ff883149cb825336fab748b61
altunenes <enesaltun2@gmail.com>
2026-02-15 17:11:55 +0300

    &session → session

--- engine/src/scramble/fourier.rs
+++ engine/src/scramble/fourier.rs
@@ -87,7 +87,7 @@ impl FourierScrambler {
         let session = load_face_detector(None)?;
         let face_regions = detect_face_regions(
             image,
-            &session,
+            session,
             face_opts.confidence_threshold,
             Some(face_opts.expansion_factor),
         )?;
